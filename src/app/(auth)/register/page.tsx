19900ec9c0e988dbc5f5a2c178b9bff4464632f9
daunampc <daunampc@gmail.com>
2025-05-01 01:23:10 +0700

    fix(Fix page auth): fix page auth login-register error import component

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from "@/components/auth/register/register-form";
+import RegisterForm from "@/components/auth/register/register-form";
 
 export default function page() {
   return (
